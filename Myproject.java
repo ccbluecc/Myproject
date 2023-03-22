edf5addb27a2fa9291d5d7fcc86cfe41b2809b7b
Chanapol <Chanapolpuntuvade1@gmail.com>
2023-03-22 16:56:58 +0700

    3 command

--- Myproject.java
+++ Myproject.java
@@ -15,6 +15,8 @@ public class Myproject {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Java");
+        System.out.println("Java Programming");
         // TODO code application logic here
     }
     
