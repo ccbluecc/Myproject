528849852f0d1ddd68e0a69023837b71a9d92de9
Chanapol <Chanapolpuntuvade1@gmail.com>
2023-03-22 16:49:36 +0700

    second command

--- Myproject.java
+++ Myproject.java
@@ -15,6 +15,7 @@ public class Myproject {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Java!");
         // TODO code application logic here
     }
     
